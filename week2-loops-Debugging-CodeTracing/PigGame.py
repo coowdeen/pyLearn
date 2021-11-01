1219742754ca7091c7b3a956e0a0546c34dcfd39
Owen Xu <xuhangcs@gmail.com>
2021-11-01 21:47:56 +0800

    a very simple pig game

---
+++ week2-loops-Debugging-CodeTracing/PigGame.py
@@ -0,0 +1,63 @@
+# play pig game
+import random
+import time
+
+def playPig():
+    # input how many players will be playing -> player
+    # each player take turns to roll the dice -> dice
+    # roll the dice until the play decides to hold -> score
+    # if the player rolls a 1, they score nothing and it becomes the next player
+    # turn;
+    # 
+
+    playerNum = 0
+    dice = 0  
+    sum = 0
+    winner = 0
+    maxScore = 0
+
+    playerNum = input("请输入玩家人数：")
+    if (input("按回车开始游戏...")):
+        print("游戏开始！")
+    for player in range(1, int(playerNum) + 1):
+        print("")
+        print(player,"号玩家回合...")
+        time.sleep(0.51)
+        sum = 0
+        while(True):
+            # roll dice
+            dice = int(random.uniform(1,6))
+            print("摇到了",dice)
+            if (dice == 1):
+                print("算了吧你，零分！下一个～")
+                print(player,"号玩家总分:", sum)
+                sum = 0
+                break
+            else:
+                sum += dice
+                if(sum >= maxScore):
+                    winner = player
+                    maxScore = sum
+                print("目前您的分数为:", sum, end=" ")
+                choice = input("还要继续吗？Y/N：")
+                if(choice == 'Y'):
+                    time.sleep(0.51)
+                    continue
+                else:
+                    time.sleep(0.3)
+                    print(player,"号玩家总分:",sum)
+                    if(player == playerNum):
+                        print("游戏结束！")
+                        print(winner,"号玩家总分：",maxScore,"获胜！")
+                    break
+    for x in range(40):
+        print("-",end="")
+    print()
+    if(maxScore == 0):
+        print("没人获胜这局打平了！")
+    else:
+        print("游戏结束！")
+        print(winner,"号玩家获胜！总分",maxScore,"分, 让我们恭喜这个比～")
+    return
+
+playPig()
