63f50b81893f004fd66391d194fecd9ddb58429a
Owen Xu <xuhangcs@gmail.com>
2021-11-01 21:57:11 +0800

    Update PigGame.py

--- week2-loops-Debugging-CodeTracing/PigGame.py
+++ week2-loops-Debugging-CodeTracing/PigGame.py
@@ -18,29 +18,34 @@ def playPig():
 
     playerNum = input("请输入玩家人数：")
     if (input("按回车开始游戏...")):
+        time.sleep(0.3)
         print("游戏开始！")
     for player in range(1, int(playerNum) + 1):
         print("")
+        time.sleep(0.3)
         print(player,"号玩家回合...")
-        time.sleep(0.51)
+        time.sleep(0.3)
         sum = 0
         while(True):
             # roll dice
             dice = int(random.uniform(1,6))
             print("摇到了",dice)
             if (dice == 1):
+                time.sleep(0.3)
                 print("算了吧你，零分！下一个～")
-                print(player,"号玩家总分:", sum)
+                time.sleep(0.3)
                 sum = 0
+                print(player,"号玩家总分:", sum)
                 break
             else:
                 sum += dice
                 if(sum >= maxScore):
                     winner = player
                     maxScore = sum
+                time.sleep(0.3)
                 print("目前您的分数为:", sum, end=" ")
                 choice = input("还要继续吗？Y/N：")
-                if(choice == 'Y'):
+                if(choice == 'Y' or choice == 'y'):
                     time.sleep(0.51)
                     continue
                 else:
